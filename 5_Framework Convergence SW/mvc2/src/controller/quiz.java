949aaf3547dd15c87418751b43bf988789f8483f
Komo1284 <136778489+Komo1284@users.noreply.github.com>
2024-03-13 22:54:41 +0900

    Add files via upload

---
+++ 5_Framework Convergence SW/mvc2/src/controller/quiz.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/quiz")
+public class quiz extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7633899842591793291L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		RequestDispatcher rd;
+		rd = req.getRequestDispatcher("/WEB-INF/test.jsp");
+		
+		rd.forward(req, resp);
+	}
+}
